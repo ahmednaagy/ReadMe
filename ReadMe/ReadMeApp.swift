f595d9be27e89c96b984df869d92f7c69704e7bd
Ahmed Nagy <ahmednaagy97@gmail.com>
2024-01-07 18:16:21 +0200

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Ahmed Nagy on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
